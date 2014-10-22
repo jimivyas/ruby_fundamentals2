016f5ef78752488682ad753858be8d3d1a4b5bd0
Jimi Vyas <jimipvyas@gmail.com>
2014-10-22 13:53:54 -0400

    fixed stuedns per cohort method

--- exercse7.rb
+++ exercse7.rb
@@ -4,12 +4,12 @@ students = {
   :cohort3 => 22
 }
 
-def students_per_cohort
-	students.each do |key_value_pair|
+def students_per_cohort(hash)
+	hash.each do |key_value_pair|
 		cohort_num = key_value_pair[0]
 		student_num = key_value_pair[1]
 		puts "#{cohort_num}: #{student_num} students"
 	end
 end
 
-students_per_cohort
+students_per_cohort(students)
