ff8539dd3c4955b824afa25b00a7450ee3097297
Jimi Vyas <jimipvyas@gmail.com>
2014-10-22 12:15:37 -0400

    added rice to list

--- exercse6.rb
+++ exercse6.rb
@@ -1,4 +1,4 @@
-grocery_list = ["kale", "carrots", "root beer", "protein powder"]
+grocery_list = ["kale", "carrots", "root beer", "protein powder", "rice"]
 
 grocery_list.each do |item|
 	puts "* #{item}"
