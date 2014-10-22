d32b79242320a634a198d195e4b5858754883415
Jimi Vyas <jimipvyas@gmail.com>
2014-10-22 12:05:52 -0400

    created exercise 6 but i spelled the name wrong on both, whoops

--- exercse5.rb
+++ exercse5.rb
@@ -1,8 +1,8 @@
-def F_to_c
+def convert
 	puts "Please enter the temperature, in Farenheit"
 	temperature = gets.chomp
 	temperature = ((temperature.to_i - 32) * 5) / 9
 	puts "That is equavalent to #{temperature} degrees Celcius"
 end
 
-F_to_c
+convert
