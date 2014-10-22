fac1f4a20a4b6d105a78a22b12be1b2fd8d98bbb
Jimi Vyas <jimipvyas@gmail.com>
2014-10-22 14:09:37 -0400

    fixed order

--- exercse7.rb
+++ exercse7.rb
@@ -1,9 +1,3 @@
-students = {
-  :cohort1 => students_increasing(34, 5),
-  :cohort2 => students_increasing(42, 5),
-  :cohort3 => students_increasing(22, 5),
-}
-
 def students_per_cohort(hash)
 	hash.each do |key_value_pair|
 		cohort_num = key_value_pair[0]
@@ -15,6 +9,14 @@ end
 def students_increasing(num, percentage)
 	return (num * (1 + (percentage * 0.01))).round #added round function beause you can't have a fractional student	
 end
+
+
+students = {
+  :cohort1 => students_increasing(34, 5),
+  :cohort2 => students_increasing(42, 5),
+  :cohort3 => students_increasing(22, 5),
+}
+
 
 students_per_cohort(students)
 puts students.keys
