3690bce90479b74bd072820d097dd8b9d9cadd0d
Jimi Vyas <jimipvyas@gmail.com>
2014-10-22 12:17:19 -0400

    added list length checker

--- exercse6.rb
+++ exercse6.rb
@@ -3,3 +3,7 @@ grocery_list = ["kale", "carrots", "root beer", "protein powder", "rice"]
 grocery_list.each do |item|
 	puts "* #{item}"
 end
+
+amount = grocery_list.count
+
+puts "There are #{amount} items on your list"
